1051ac8596d0f60b02ebd99cabb536be02454237
Biiddd <1256773838@qq.com>
2024-07-02 22:45:32 +0800

    test(showFinalScore.vue): 接口测试完成

--- src/components/ShowFinalScore.vue
+++ src/components/ShowFinalScore.vue
@@ -2,20 +2,19 @@
 import http from "@/http";
 import { ref } from "vue";
 
-const finalScore = ref<number>();
+const finalScore = ref<string>();
+const user_id = 111111111111
 
-console.log("finalScore", finalScore);
-
-http.get("/stu/getScore").then((res) => {
+http.post("/stu/getScore", {user_id: user_id}).then((res) => {
   finalScore.value = res.data.finalScore;
 });
 </script>
 
 <template>
-  <template v-if="finalScore === -1">
+  <template v-if="finalScore === '-1'">
     <a-empty />
   </template>
-  <template v-if="finalScore !== -1">
+  <template v-if="finalScore !== '-1'">
     <a-result status="success" :title="'最终成绩为：' + finalScore" />
   </template>
 </template>
