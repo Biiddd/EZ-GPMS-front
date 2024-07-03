39609e76822bb275d0d9666ef9869ea8e36b65a4
Biiddd <1256773838@qq.com>
2024-07-04 03:06:00 +0800

    fix(ShowFinal): 修复总评页面无法显示的问题

--- src/components/ShowFinalScore.vue
+++ src/components/ShowFinalScore.vue
@@ -1,24 +1,22 @@
 <script lang="ts" setup>
 import http from '@/http';
-import { ref } from 'vue';
-import { ScoreData } from '@/utils/ScoreData';
 
-const thisScore = ref<ScoreData>();
 const user_id = 111111111111;
+let finalScore: null | number
 
 http.post('/stu/getScore', { user_id: user_id }).then((res) => {
-  thisScore.value = res.data;
+  finalScore = res.data.finalScore;
 });
 </script>
 
 <template>
-  <template v-if="thisScore.finalScore === null">
+  <template v-if="finalScore === null || -1 || undefined">
     <a-result status="info">
       <template #title> 总评成绩尚未出炉<br />请在成绩页面查看其他得分详情 </template>
     </a-result>
   </template>
-  <template v-if="thisScore.finalScore !== -1">
-    <a-result status="success" :title="'最终成绩为：' + thisScore.finalScore" />
+  <template v-else>
+    <a-result status="success" :title="'最终成绩为：' + finalScore" />
   </template>
 </template>
 
