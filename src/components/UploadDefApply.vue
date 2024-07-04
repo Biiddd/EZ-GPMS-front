1372488e6147cae356b0bd5301863600f1d4d735
Biiddd <1256773838@qq.com>
2024-07-04 19:10:48 +0800

    refactor(UploadDefApply): 重构答辩申请上传逻辑

---
+++ src/components/UploadDefApply.vue
@@ -0,0 +1,41 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+import { UploadOutlined } from '@ant-design/icons-vue';
+import { message } from 'ant-design-vue';
+
+const labelCol = { style: { width: '150px' } };
+const wrapperCol = { span: 14 };
+
+const defApply = ref([]);
+
+const uploadDefApply = (info: any) => {
+  if (info.file.status === 'done') {
+    message.success('答辩申请上传成功', 5);
+  } else if (info.file.status === 'error') {
+    message.error('答辩申请失败, 请重新上传', 5);
+  }
+};
+</script>
+
+<template>
+  <a-form
+    :label-col="labelCol"
+    :wrapper-col="wrapperCol"
+    layout="horizontal"
+    style="max-width: 600px">
+    <a-form-item :wrapper-col="{ offset: 14, span: 16 }">
+      <a-upload
+        v-model:file-list="defApply"
+        list-type="picture"
+        accept=".pdf"
+        @change="uploadDefApply"
+        :data="{ stu_id: '111111111111', filename: '答辩申请表' }"
+        action="http://127.0.0.1:5174/api/upload">
+        <a-button size="large" v-if="defApply.length < 1">
+          <upload-outlined />
+          选择答辩申请表
+        </a-button>
+      </a-upload>
+    </a-form-item>
+  </a-form>
+</template>
