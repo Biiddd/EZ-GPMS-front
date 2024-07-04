fa18a8009775163e185a4920bf4b9235a6f77e4a
Biiddd <1256773838@qq.com>
2024-07-04 23:32:22 +0800

    feat(Login): 添加登录界面

--- src/router/index.js
+++ src/router/index.js
@@ -1,77 +1,78 @@
-import { createRouter, createWebHistory } from "vue-router";
-import Home from "@/views/Home.vue";
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '@/views/Home.vue';
 import StuProcess from '@/views/stu/StuProcess.vue';
 import SignScore from '@/views/teach/SignScore.vue';
+import Login from '@/views/Login.vue';
 
 const routes = [
   {
-    path: "/",
-    name: "default",
-    component: Home,
+    path: '/',
+    name: 'login',
+    component: Login
   },
   {
-    path: "/home",
-    name: "Home",
+    path: '/home',
+    name: 'Home',
     component: Home,
-    redirect: "/home",
+    redirect: '/home',
     children: [
       {
-        path: "/process",
-        name: "process",
-        component: StuProcess,
+        path: '/process',
+        name: 'process',
+        component: StuProcess
       },
       {
-        path: "/grade",
-        name: "grade",
-        component: () => import("@/views/stu/GradeTable.vue"),
+        path: '/grade',
+        name: 'grade',
+        component: () => import('@/views/stu/GradeTable.vue')
       },
       {
-        path: "/info",
-        name: "info",
-        component: () => import("@/views/Info.vue"),
+        path: '/info',
+        name: 'info',
+        component: () => import('@/views/Info.vue')
       },
       {
-        path: "/editInfo",
-        name: "editInfo",
-        component: () => import("@/components/InfoEditor.vue"),
+        path: '/editInfo',
+        name: 'editInfo',
+        component: () => import('@/components/InfoEditor.vue')
       },
       {
-        path: "/changePasswd",
-        name: "changePasswd",
-        component: () => import("@/views/ChangePasswd.vue"),
+        path: '/changePasswd',
+        name: 'changePasswd',
+        component: () => import('@/views/ChangePasswd.vue')
       },
       {
-        path: "/showStu",
-        name: "showStu",
-        component: () => import("@/views/teach/ShowStuList.vue"),
+        path: '/showStu',
+        name: 'showStu',
+        component: () => import('@/views/teach/ShowStuList.vue')
       },
       {
-        path: "/uploadRequire",
-        name: "uploadRequire",
-        component: () => import("@/views/teach/UploadRequirement.vue"),
-      },
-              {
-        path: "/groupArrange",
-        name: "groupArrange",
-        component: () => import("@/components/GroupArrange.vue"),
+        path: '/uploadRequire',
+        name: 'uploadRequire',
+        component: () => import('@/views/teach/UploadRequirement.vue')
       },
       {
-        path: "/signScore",
-        name: "signScore",
-        component: SignScore,
+        path: '/groupArrange',
+        name: 'groupArrange',
+        component: () => import('@/components/GroupArrange.vue')
+      },
+      {
+        path: '/signScore',
+        name: 'signScore',
+        component: SignScore
       }
-    ],
+    ]
   },
   {
-    path: "/teacherHome",
-    name: "teacherHome",
-    component: () => import("@/views/TeacherHome.vue"),
+    path: '/teacherHome',
+    name: 'teacherHome',
+    component: () => import('@/views/TeacherHome.vue')
   }
 ];
 
 const router = createRouter({
-  history: createWebHistory("/home"),
-  routes,
+  history: createWebHistory('/'),
+  routes
 });
 
 export default router;
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,164 @@
-<script setup lang="ts">
+<script setup>
+import { reactive, onMounted } from 'vue';
+// import { updateUserState } from "@/auth.js";
+import router from '@/router/index.js';
+import http from '@/utils/http';
 
+const formState = reactive({
+  username: '',
+  password: '',
+  remember: true
+});
+
+// 在组件加载时，从本地存储中读取用户名和密码
+onMounted(() => {
+  const storedUsername = localStorage.getItem('rememberedUsername');
+  const storedPassword = localStorage.getItem('rememberedPassword');
+
+  // 如果存储中有数据，填充到表单中，并设置“记住我”选项为true
+  if (storedUsername && storedPassword) {
+    formState.username = storedUsername;
+    formState.password = storedPassword;
+    formState.remember = true;
+  }
+});
+
+const onFinish = async () => {
+  try {
+    const response = await http.post('/login', formState);
+
+    if (response.data.success) {
+      //登录成功，获取服务器返回的用户信息
+      const userInfo = formState.username;
+      // 更新用户状态
+      updateUserState(true, userInfo);
+
+      // 如果选择了“记住我”，将用户名和密码存储到本地存储中
+      if (formState.remember) {
+        localStorage.setItem('rememberedUsername', formState.username);
+        localStorage.setItem('rememberedPassword', formState.password);
+        localStorage.setItem('isLoggedIn', true);
+      } else {
+        // 如果没有选择“记住我”，清除本地存储中的用户名和密码
+        localStorage.removeItem('rememberedUsername');
+        localStorage.removeItem('rememberedPassword');
+      }
+      // 跳转到首页
+      await router.push({ name: 'Home' });
+    } else {
+      // 账号密码错误，弹出错误信息
+      alert(response.data.message || '用户名或密码错误');
+    }
+  } catch (error) {
+    alert('登录失败，请稍后再试');
+  }
+};
+
+const onFinishFailed = (errorInfo) => {};
 </script>
 
 <template>
+  <div class="login">
+    <div class="login-box">
+      <div class="top">
+        <div class="logo"></div>
+      </div>
+      <div class="mid">
+        <a-form
+          :model="formState"
+          name="basic"
+          :label-col="{ span: 8 }"
+          :wrapper-col="{ span: 10 }"
+          autocomplete="off"
+          @finish="onFinish"
+          @finishFailed="onFinishFailed">
+          <a-form-item
+            class="custom-label"
+            label="用户名"
+            name="username"
+            :rules="[{ required: true, message: '请输入用户名!' }]">
+            <a-input v-model:value="formState.username" />
+          </a-form-item>
 
+          <a-form-item
+            label="密码"
+            name="password"
+            :rules="[{ required: true, message: '请输入密码!' }]">
+            <a-input-password v-model:value="formState.password" />
+          </a-form-item>
+
+          <a-form-item name="remember" :wrapper-col="{ offset: 10, span: 16 }">
+            <a-checkbox v-model:checked="formState.remember">记住我</a-checkbox>
+          </a-form-item>
+
+          <a-form-item :wrapper-col="{ offset: 10, span: 16 }">
+            <div class="item-btn">
+              <a-button type="primary" html-type="submit">登录</a-button>
+            </div>
+          </a-form-item>
+        </a-form>
+      </div>
+      <div class="bottom">Copyright © 2024 Biid</div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
+.login {
+  width: 100%;
+  height: 100%;
+  background: url('../../assets/login-bg.png') no-repeat;
+  background-size: cover;
+  position: fixed;
 
+  .login-box {
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    height: 100%;
+    padding-top: 10%;
+
+    .top {
+      margin-bottom: 30px;
+      text-align: center;
+
+      .logo {
+        font-size: 0;
+        max-width: 50%;
+        margin: 0 auto;
+      }
+    }
+
+    .mid {
+      font-size: 14px;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+
+      .item-btn {
+        width: 410px;
+        margin-top: 20px;
+
+        input {
+          border: 0;
+          width: 100%;
+          height: 40px;
+          background: #1f87e8;
+          color: #fff;
+          border-radius: 3px;
+        }
+      }
+    }
+
+    .bottom {
+      position: absolute;
+      bottom: 10%;
+      width: 100%;
+      color: #999;
+      font-size: 12px;
+      text-align: center;
+    }
+  }
+}
 </style>
