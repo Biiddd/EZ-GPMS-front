ea6036c1b4f59572a68583749afeaf6591f81ea0
Biiddd <1256773838@qq.com>
2024-07-07 19:33:43 +0800

    perf(InfoEditor): 增加信息修改确认弹窗

--- src/components/InfoEditor.vue
+++ src/components/InfoEditor.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import { RegionSelects } from 'v-region';
 import http from '@/utils/http';
 import { thisEditUserInfo } from '@/utils/UserInfo';
@@ -11,6 +11,21 @@ const wrapperCol = { span: 14 };
 
 const emailRegex: RegExp = /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
 const telRegex: RegExp = /^(13[0-9]|14[5-9]|15[0-3,5-9]|16[6]|17[0-8]|18[0-9]|19[8,9])\d{8}$/;
+
+const confirmText = ref<string>('是否确认修改修改信息?');
+const confirmLoading = ref<boolean>(false);
+const isShowConfirmDialog = ref<boolean>(false);
+const showConfirmDialog = () => {
+  isShowConfirmDialog.value = true;
+};
+
+const handleOk = () => {
+  confirmText.value = '提交中...';
+  confirmLoading.value = true;
+  onSubmitClick().finally(() => {
+    confirmLoading.value = false;
+  });
+};
 
 function validFormData(): boolean {
   if (!emailRegex.test(thisEditUserInfo.value.email)) {
@@ -28,6 +43,7 @@ const onSubmitClick = async () => {
   if (validFormData()) {
     try {
       await http.put('/updateInfo', thisEditUserInfo.value);
+      isShowConfirmDialog.value = false;
       message.success('修改成功');
     } catch (error) {
       message.error('未知错误, 请重试');
@@ -65,7 +81,14 @@ onMounted(() => {
     </a-form-item>
 
     <a-form-item :wrapper-col="{ offset: 6, span: 6 }">
-      <a-button type="primary" @click="onSubmitClick">确认编辑</a-button>
+      <a-button type="primary" @click="showConfirmDialog">确认编辑</a-button>
+      <a-modal
+        v-model:open="isShowConfirmDialog"
+        title="提交确认"
+        :confirm-loading="confirmLoading"
+        @ok="handleOk">
+        <p>{{ confirmText }}</p>
+      </a-modal>
     </a-form-item>
   </a-form>
 </template>
