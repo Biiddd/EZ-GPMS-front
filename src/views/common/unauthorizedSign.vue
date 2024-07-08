1c2d2291b1cd823acbafdd3441c96bb077f860ae
Biiddd <1256773838@qq.com>
2024-07-08 15:34:34 +0800

    perf(UnAuthorizedSign): 修改文字描述

--- src/views/common/unauthorizedSign.vue
+++ src/views/common/unauthorizedSign.vue
@@ -3,7 +3,7 @@ import { getUserInfo, isLoggedIn } from '@/utils/auth';
 </script>
 
 <template>
-  <a-result status="403" title="您暂时无权对该学生进行打分" sub-title="请等待上一阶段评分完成">
+  <a-result status="403" title="您暂时无权对该学生进行打分" sub-title="请等待同组教师完成评分">
     <template #extra>
       <p>当前您的学工号为: {{ getUserInfo().user_id }}</p>
       <p>您的用户类型为: {{ getUserInfo().user_type }}</p>
